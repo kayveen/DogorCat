2b16ddecc4a5eea9fd105d4bba396e56eca04f47
Kevin Degila <degilakevin@gmail.com>
2019-04-10 14:21:21 +0100

    Used Pretrained CNN model trained to  kaggle dogs vs cats dataset

--- app.py
+++ app.py
@@ -1,11 +1,78 @@
 import os
 from flask import Flask, render_template, request, send_from_directory
-from keras.models import load_model
 from keras_preprocessing import image
 import numpy as np
 import tensorflow as tf
 
-
 app = Flask(__name__)
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+STATIC_FOLDER = 'static'
+# Path to the folder where we'll store the upload before prediction
+UPLOAD_FOLDER = STATIC_FOLDER + '/uploads'
+# Path to the folder where we store the different models
+MODEL_FOLDER = STATIC_FOLDER + '/models'
+
+
+def load__model():
+    """Load model once at running time for all the predictions"""
+    print('[INFO] : Model loading ................')
+    global model
+    model = tf.keras.models.load_model(MODEL_FOLDER + '/catsVSdogs.h5')
+    global graph
+    graph = tf.get_default_graph()
+    print('[INFO] : Model loaded')
+
+
+def predict(fullpath):
+    data = image.load_img(fullpath, target_size=(150, 150, 3))
+    # (150,150,3) ==> (1,150,150,3)
+    data = np.expand_dims(data, axis=0)
+    # Scaling
+    data = data.astype('float') / 255
+
+    # Prediction
+
+    with graph.as_default():
+        result = model.predict(data)
+
+    return result
+
+
+# Home Page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Process file and predict his label
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        file = request.files['image']
+        fullname = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(fullname)
+
+        result = predict(fullname)
+
+        pred_prob = result.item()
+
+        if pred_prob > .5:
+            label = 'Dog'
+            accuracy = round(pred_prob * 100, 2)
+        else:
+            label = 'Cat'
+            accuracy = round((1 - pred_prob) * 100, 2)
+
+        return render_template('predict.html', image_file_name=file.filename, label=label, accuracy=accuracy)
+
+
+@app.route('/upload/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+
+if __name__ == '__main__':
+    load__model()
+    app.run(debug=True)
